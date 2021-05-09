d814eee2e308499bb5a93b109430399af3cd68af
ryan <ryan_running@163.com>
2021-05-09 18:36:15 +0800

    admin站点

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -6,8 +6,14 @@ from django.db import models
 class BookInfo(models.Model):
     name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 
 class PeopleInfo(models.Model):
     name = models.CharField(max_length=10)
     gender = models.BooleanField()
     book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
