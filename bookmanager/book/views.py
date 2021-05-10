b4891886ff09f0201bd02dc58f17ec72de9bafc4
ryan <ryan_running@163.com>
2021-05-10 09:59:08 +0800

    aaaaa

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+# 导入HTTPResponse模块
+from django.http import HttpResponse
+
+# 定义试图函数
+
+
+def index(request):
+    return HttpResponse('OK')
+
